29553e6e7ce4074bbeacdec22bd3256dfb31390c
lehaianh3112 <1851161833@e.tlu.edu.vn>
2021-11-04 21:10:15 +0700

    calculator react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-eval */
+import './calculator.css';
+import React,{useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [result,setResult]=useState("");
+
+  const myFunction = (e) => {
+    setResult(result.concat(e.target.name))
+  }
+  const clear=()=>{
+    setResult("");
+  }
+  const del=()=>{
+    setResult(result.slice(0,-1));
+  }
+  const calculation=()=>{
+    try {
+      setResult(eval(result));
+    } catch (error) {
+      setResult("Error")
+    }
+  }
+   return (
+  <div className="container">
+    <div className="calculator">
+    <input type="text" placeholder="0" value={result} />
+    <button className="btn btn-primary" id="clear" onClick={clear}>AC</button>
+            <button className="btn btn-primary" onClick={del}>DEL</button>
+            <button name="%" className="btn btn-primary" onClick={myFunction}>%</button>
+            <button name="+"  className="btn btn-primary" onClick={myFunction}>+</button>
+            <button name="1"  onClick={myFunction}>1</button>
+            <button name="2"  onClick={myFunction}>2</button>
+            <button name="3" onClick={myFunction}>3</button>
+            <button name="-"  className="btn btn-primary" onClick={myFunction} >-</button>
+            <button name="4"  onClick={myFunction}>4</button>
+            <button name="5"  onClick={myFunction}>5</button>
+            <button name="6" onClick={myFunction}>6</button>
+            <button name="*"  className="btn btn-primary" onClick={myFunction}>*</button>
+            <button name="7" onClick={myFunction}>7</button>
+            <button name="8"  onClick={myFunction}>8</button>
+            <button name="9"  onClick={myFunction}>9</button>
+            <button name="/"  className="btn btn-primary" onClick={myFunction}>/</button>
+            <button name="0"  onClick={myFunction}>0</button>
+            <button name="."  className="btn btn-primary" onClick={myFunction}>.</button>
+            <button className="result btn btn-primary" onClick={calculation}>=</button>
     </div>
+  </div>
   );
 }
 
